7ca41ae9fad696488ffe851f29df9ebf920ce44a
ValloonSweet <valloon91105@gmail.com>
2022-10-29 16:13:59 +0300

    add: ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @Output() addIngredient = new EventEmitter<Ingredient>();
+
+  @ViewChild('amountInput')
+  amountInput: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddIngredient(nameInput: HTMLInputElement) {
+    console.log(nameInput.value);
+    console.log(this.amountInput.nativeElement.value);
+    this.addIngredient.emit({
+      name: nameInput.value,
+      amount: this.amountInput.nativeElement.value
+    })
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addNewIngredient(newIngredient: Ingredient) {
+    this.ingredients.push(newIngredient);
+  }
+
 }
