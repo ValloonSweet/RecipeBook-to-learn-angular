5a1abf39231b1709d7397dd9abb897077b41a4c0
ValloonSweet <valloon91105@gmail.com>
2022-10-31 10:49:05 +0300

    enable dropdown

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, OnInit } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+}) 
+export class DropdownDirective implements OnInit {
+    
+    constructor() {
+
+    }
+
+    ngOnInit(): void {
+        
+    }
+
+}
